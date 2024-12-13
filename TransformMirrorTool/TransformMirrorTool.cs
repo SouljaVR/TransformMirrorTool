b0367d7bb0a5d903ee5470194ff0791b7f02aa95
BigSoulja <97592971+SouljaVR@users.noreply.github.com>
2024-12-13 19:45:34 +0000

    Update TransformMirrorTool.cs

--- TransformMirrorTool/TransformMirrorTool.cs
+++ TransformMirrorTool/TransformMirrorTool.cs
@@ -2,8 +2,6 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
-using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class TransformPair
